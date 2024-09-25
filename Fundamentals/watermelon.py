9f14da7417137a9a3f9d90e26f17a6d84c8ac19c
Choroyo <bryanchora@gmail.com>
2024-09-24 21:23:31 -0500

    edit comments

---
+++ Fundamentals/watermelon.py
@@ -0,0 +1,10 @@
+#Enter the kg of the watermelon
+weight = int(input())
+
+#Check if the watermelon weight is even and also in even weight
+if weight % 2 == 0 and weight != 2:
+    #if is true print yes
+    print('yes')
+else:
+    #else print no
+    print('no')
